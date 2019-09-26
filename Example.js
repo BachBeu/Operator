efac3219e1a7b6dfffae5277f58c926b431afd6f
Hellu Shiro! <katobmt107@gmail.com>
2019-09-26 11:50:58 +0700

    2

---
+++ Example.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("enter the width");
+inputHeight = prompt("enter the height");
+
+width = parseInt(inputWidth);
+height = parseInt(inputHeight);
+
+let area = width * height;
+document.write('the area is :' + area);
